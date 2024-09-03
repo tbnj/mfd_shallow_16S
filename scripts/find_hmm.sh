9ced16aac91fe2c289d31788f47a77d5fb380517
Thomas Nymann <tbnj@bio.aau.dk>
2024-09-03 20:52:32 +0200

    Small changes to find/nhmmer script

--- scripts/find_hmm.sh
+++ scripts/find_hmm.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/env bash
 #### Script to search for and the creation of symbolic links to nhmmer output ####
 
-# paths
+# Paths
 DIR_IN=('/mfd_shallow_16S/data')
 DIR_OUT_F=('/mfd_shallow_16S/data/hmm_forward_out/')
 DIR_OUT_R=('/mfd_shallow_16S/data/hmm_reverse_out/')
 
-# find samples - add min and max depth  
+# Find results for ARC and BAC
 find $DIR_IN -type f -name 'arc_*_forward.hmmout.txt' -exec ln -s '{}' $DIR_OUT_F ';'
 find $DIR_IN -type f -name 'bac_*_forward.hmmout.txt' -exec ln -s '{}' $DIR_OUT_F ';'
-find $DIR_IN -type f -name 'euk_*_forward.hmmout.txt' -exec ln -s '{}' $DIR_OUT_F ';'
 find $DIR_IN -type f -name 'arc_*_reverse.hmmout.txt' -exec ln -s '{}' $DIR_OUT_R ';'
-find $DIR_IN -type f -name 'bac_*_reverse.hmmout.txt' -exec ln -s '{}' $DIR_OUT_F ';'
-find $DIR_IN -type f -name 'euk_*_reverse.hmmout.txt' -exec ln -s '{}' $DIR_OUT_F ';'
+find $DIR_IN -type f -name 'bac_*_reverse.hmmout.txt' -exec ln -s '{}' $DIR_OUT_R ';'
+
+# Find results for EUK
+find $DIR_IN -type f -name 'euk_*_forward.hmmout.txt' -exec ln -s '{}' $DIR_OUT_F ';'
+find $DIR_IN -type f -name 'euk_*_reverse.hmmout.txt' -exec ln -s '{}' $DIR_OUT_R ';'
